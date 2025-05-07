915f9c46f07ea68cde54011f1a1099802b4a27b0
youssef1G <140439663+youssef1G@users.noreply.github.com>
2025-05-07 23:34:00 +0300

    Update Mosaab.js

--- IT-Project/Mosaab.js
+++ IT-Project/Mosaab.js
@@ -1,7 +1,7 @@
-let TheLanguge = "en";
+let TheLanguage = "en";
 
-function Languge() {
-    if (TheLanguge === "en") {
+function language() {
+    if (TheLanguage === "en") {
         document.getElementById("WELCOME").innerText = "اهلا بك في V8 ZONE";
 
         document.getElementById("Paragraph").innerText = "اكتشف قوة السيارات وأسلوبها وإرثها من الأيقونات الكلاسيكية إلى الوحوش الحديثة - نقدم لك المواصفات التفصيلية والمراجعات وكل ما تحتاج إلى معرفته حول سياراتك المفضلة.";
@@ -12,7 +12,7 @@ function Languge() {
         document.getElementById("AboutUs").innerText = "من نحن";
         document.getElementById("ContactUs").innerText = "تواصل معنا";
         document.getElementById("LogOut").innerText = "تسجيل الخروج";
-        document.getElementById("Languge").innerText = "اللغات";
+        document.getElementById("language").innerText = "اللغات";
         document.getElementById("Mazda").innerText = "مازدا";
         document.getElementById("Nissan").innerText = "نيسان";
         document.getElementById("Porsche").innerText = "بورش";
@@ -20,8 +20,8 @@ function Languge() {
         document.getElementById("Chevorlet").innerText = "شيفروليه";
         document.getElementById("Mercedes").innerText = "مرسيدس";
 
-        TheLanguge = "ar";
-    } else if (TheLanguge === "ar") {
+        TheLanguage = "ar";
+    } else if (TheLanguage === "ar") {
         document.getElementById("WELCOME").innerText = "WILLKOMMEN BEI V8 ZONE";
 
         document.getElementById("Paragraph").innerText = "Entdecken Sie die Kraft, den Stil und das Erbe der Autos – von klassischen Ikonen bis zu modernen Bestien. Wir bieten Ihnen detaillierte Spezifikationen, Bewertungen und alles, was Sie über Ihre Lieblingsautos wissen müssen.";
@@ -32,7 +32,7 @@ function Languge() {
         document.getElementById("AboutUs").innerText = "Über uns";
         document.getElementById("ContactUs").innerText = "Kontakt";
         document.getElementById("LogOut").innerText = "Abmelden";
-        document.getElementById("Languge").innerText = "Sprachen";
+        document.getElementById("language").innerText = "Sprachen";
         document.getElementById("Mazda").innerText = "Mazda";
         document.getElementById("Nissan").innerText = "Nissan";
         document.getElementById("Porsche").innerText = "Porsche";
@@ -40,7 +40,7 @@ function Languge() {
         document.getElementById("Chevorlet").innerText = "Chevrolet";
         document.getElementById("Mercedes").innerText = "Mercedes";
 
-        TheLanguge = "de";
+        TheLanguage = "de";
     } else {
         document.getElementById("WELCOME").innerText = "WELCOME TO V8 ZONE";
 
@@ -52,7 +52,7 @@ function Languge() {
         document.getElementById("AboutUs").innerText = "About Us";
         document.getElementById("ContactUs").innerText = "Contact Us";
         document.getElementById("LogOut").innerText = "Log Out";
-        document.getElementById("Languge").innerText = "Language";
+        document.getElementById("language").innerText = "Language";
         document.getElementById("Mazda").innerText = "Mazda";
         document.getElementById("Nissan").innerText = "Nissan";
         document.getElementById("Porsche").innerText = "Porsche";
@@ -60,6 +60,6 @@ function Languge() {
         document.getElementById("Chevorlet").innerText = "Chevorlet";
         document.getElementById("Mercedes").innerText = "Mercedes";
 
-        TheLanguge= "en";
+        TheLanguage= "en";
     }
 }
